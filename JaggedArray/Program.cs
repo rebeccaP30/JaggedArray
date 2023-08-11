e97fd6979cc1739fa36711d070a53e2f4c1172e4
Rebecca.Phakathi <Rebecca.Phakathi@JHBHO-REBECCAP>
2023-08-11 14:47:09 +0200

    Add project files.

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare jaggedArray
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1, 2, 3 };
+            jaggedArray[2] = new int[] { 13, 21 };
+
+            // alternative way:
+            int[][] jaggedArray2 = new int[][]
+            {
+            new int[] { 2, 3, 5, 7, 11 },
+            new int[] { 1, 2, 3 }
+            };
+
+            Console.WriteLine("The Value in the middle of the first entry is {0}", jaggedArray2[0][2]);
+
+            for(int i = 0; i < jaggedArray2.Length; i++)
+            {
+                Console.WriteLine("Element {0}", i);
+                for(int j = 0; j < jaggedArray2[i].Length; j++)
+                    Console.WriteLine("{0} ", jaggedArray2[i][j]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
